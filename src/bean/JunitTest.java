4be565be254789d62a8c79dd4e6408d2fe8519fc
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-05-05 12:48:36 +0530

    Add files via upload

---
+++ src/bean/JunitTest.java
@@ -0,0 +1,26 @@
+package bean;
+import static net.sourceforge.jwebunit.junit.JWebUnit.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class JunitTest {
+
+		@Before
+		public void setup(){
+			setBaseUrl("http://localhost:8081/CustomerOnBoard/");
+			//http://10.0.0.116:8080/job/JunitTest1/ws/WebContent/
+		}
+		@Test
+		public void test(){
+			beginAt("index1.jsp"); 
+	        
+	        assertTitleEquals("Login page");
+	       // setTextField("Uname", "admin");
+	       // setTextField("Password", "admin");
+	      clickLink("register");
+	        //submit();
+	        assertTitleEquals("Registration Form");
+		}
+		
+}
