a9fa3beb797b3c9143b4a54d27c029577eb64f48
BlackCompetencyBuilding <md.raghib0101@gmail.com>
2017-05-06 10:28:42 +0200

    Update JunitTest.java

--- src/bean/JunitTest.java
+++ src/bean/JunitTest.java
@@ -1,28 +1,14 @@
 package bean;
-import static net.sourceforge.jwebunit.junit.JWebUnit.*;
-
-import org.junit.Before;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.junit.Test;
 
-public class JunitTest {
-
-		@Before
-		public void setup(){
-			URLConnection openConnection = new URL("http://10.0.0.108:8080/job/JunitTest1/ws/WebContent").openConnection();
-		openConnection.addRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:25.0) Gecko/20100101 Firefox/25.0");
-			//setBaseUrl("http://10.0.0.108:8080/job/JunitTest1/ws/WebContent");
-			//http://10.0.0.116:8080/job/JunitTest1/ws/WebContent/
-		}
-		@Test
-		public void test(){
-			beginAt("index1.jsp"); 
-	        
-	        assertTitleEquals("Login page");
-	       // setTextField("Uname", "admin");
-	       // setTextField("Password", "admin");
-	      clickLink("register");
-	        //submit();
-	        assertTitleEquals("Registration Form");
-		}
-		
-}
+public class JunitTest{
+  @Test
+  public void test(){
+   WebDriver driver = new FirefoxDriver();
+   driver.get("http://10.0.0.116:8080/job/JunitTest1/ws/WebContent/index1.jsp");
+  Assert.assertTrue(driver.getTitle().contains("Login Page"));
+    }
+ }
